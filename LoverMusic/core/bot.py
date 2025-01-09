a277a3c6c6916a76d291863fd53dd1a2a2b3ab38
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-09 15:02:14 +0530

    Update bot.py

--- LoverMusic/core/bot.py
+++ LoverMusic/core/bot.py
@@ -14,7 +14,7 @@ import config
 from ..logging import LOGGER
 
 
-class LoverMusic (Client):
+class LoverMusic(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
