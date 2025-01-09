7c3be4e6cf82f95d5b652d846532b193343ad2a2
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2025-01-09 17:30:57 +0530

    Update call.py

--- LoverMusic/core/call.py
+++ LoverMusic/core/call.py
@@ -36,7 +36,7 @@ from LoverMusic.utils.database import (
 from LoverMusic.utils.exceptions import AssistantErr
 from LoverMusic.utils.inline.play import stream_markup
 from LoverMusic.utils.stream.autoclear import auto_clean
-from LoverMusic.utils.thumbnails 
+from LoverMusic.utils.thumbnails import get_thumb
 from strings import get_string
 
 autoend = {}
